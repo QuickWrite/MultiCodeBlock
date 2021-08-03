5da87ddd77b7c419c5eda2c76ebdf4dded9c9ff6
QuickWrite <54590845+QuickWrite@users.noreply.github.com>
2021-08-03 20:21:07 +0200

    Replaced recursiveTagParse
    Replaced recursiveTagParse with recursiveTagParseFully as the Documentation states that not all of the MediaWiki Syntax is parsed and things like links are left out.
    This is not useful for the extension as the extension relies on the way that everything can be displayed in these sections to explain the code.
    
    /**
     * Half-parse wikitext to half-parsed HTML. This recursive parser entry point
     * can be called from an extension tag hook.
     *
     * The output of this function IS NOT SAFE PARSED HTML; it is "half-parsed"
     * instead, which means that lists and links have not been fully parsed yet,
     * and strip markers are still present.
     *
     * Use recursiveTagParseFully() to fully parse wikitext to output-safe HTML.
     *
     * Use this function if you're a parser tag hook and you want to parse
     * wikitext before or after applying additional transformations, and you
     * intend to *return the result as hook output*, which will cause it to go
     * through the rest of parsing process automatically.
     *
     * If $frame is not provided, then template variables (e.g., {{{1}}}) within
     * $text are not expanded
     */

--- includes/MultiCodeBlock.php
+++ includes/MultiCodeBlock.php
@@ -120,7 +120,7 @@ function &combineCodeDescription(string $code, Description &$desc, Parser &$pars
             $i++;
         }
 
-        $return .= '</pre></ol></th><th class="second">' . $parser->recursiveTagParse($desc->texts[$j]) . '</td>';
+        $return .= '</pre></ol></th><th class="second">' . $parser->recursiveTagParseFully($desc->texts[$j]) . '</td>';
     }
 
     $return .= '</table>';
